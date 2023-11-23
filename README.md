b811708eeb10abda6b451cd9ba2b70440cf1dabd
Michael Koza <michael.koza@mailbox.org>
2023-11-23 22:23:31 +0100

    Video 3 Bootloader: Projekt in bootloader und firmware getrennt. linkerscript für bootloader und app angepasst.

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ explanation about PWM --> freq depends on the ARR and prescaler
 I have written to function, one for setup the TIMER and enable the timer, configure the pwm stuff
 
 second: function, that can set the duty_cycle
+
+
+## Video 4: Bootloader
+
+We have split the app in bootloader and app
+
+We are writting a simple bootloader -> Bootloader is a piece of software, which jumps to a specific address with the main app
+
+- Prevent linking when the bootloader is larger than maximum allowed size -> change the linker script from 512kb to 32kb
+- Pad the bootloader  to max size -> with a python script the free space will be filled up with 0xff
--- app/src/firmware.c
+++ app/src/firmware.c
@@ -2,10 +2,17 @@
 #include <libopencm3/stm32/gpio.h>
 #include <core/system.h>
 #include <core/timer.h>
+#include <libopencm3/cm3/scb.h>
+
+#define BOOTLOADER_SIZE	(0x8000U)
+
 
 #define LED_PORT	(GPIOA)
 #define LED			(GPIO5)
 
+static void vector_setup(void) {
+	SCB_VTOR = BOOTLOADER_SIZE;
+}
 
 static void gpio_setup(void) {
 	rcc_periph_clock_enable(RCC_GPIOA);
@@ -16,6 +23,7 @@ static void gpio_setup(void) {
 
 
 int main(void) {
+	vector_setup();
 	system_setup();
 	gpio_setup();
 	timer_setup();
@@ -25,9 +33,9 @@ int main(void) {
 	timer_pwm_set_duty_cycle(duty_cycle);
 	while(1) {
 		if (system_get_ticks() - start_time >= 200) {
-			duty_cycle -= 10.0f;
-			if(duty_cycle < 1.0f) {
-				duty_cycle = 100.0f;
+			duty_cycle += 10.0f;
+			if(duty_cycle > 100.0f) {
+				duty_cycle = 0.0f;
 			}
 			timer_pwm_set_duty_cycle(duty_cycle);
 			start_time = system_get_ticks();
